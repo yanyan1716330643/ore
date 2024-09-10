1dceae02f8f8fb7d99d8f7fe9ef6afbb274084f3
yan.yan <18326000767@163.com>
2024-09-10 11:31:26 +0800

    feat: 优化init

--- test/src/client/process_initialize.ts
+++ test/src/client/process_initialize.ts
@@ -25,7 +25,7 @@ import {
 
 
 
-async function main() {
+async function process_initialize() {
   //获取合约公钥
   let programId = await loadProgramId();
   //获取初始化用户密钥对(最高权限的那个账户)
@@ -43,14 +43,15 @@ async function main() {
   let bumps = new Array(13);
   //account
   let keys = new Array(19);
-
+  //操作
+  let func = OreInstruction.Initialize;
 
   // 填充keys
   for (let i = 0; i < keys.length; i++) {
     //signer account
     if (i==0){
       keys[i]={pubkey: userKeypair.publicKey, isSigner: true, isWritable: true}
-      bumps[i] =  OreInstruction.Initialize;
+      bumps[i] =  func.value;
     }
 
     //bus0~7 account pda
@@ -65,6 +66,7 @@ async function main() {
       const [pda, bump] = await PublicKey.findProgramAddress([seeds], programId);
       bumps[i] = bump;
       keys[i] = {pubkey: pda, isSigner: false, isWritable: true}
+      console.log(pda.toBase58(),bump,"pda bus"+(i-1))
     }
 
     //config account pda
@@ -73,7 +75,7 @@ async function main() {
       const [pda, bump] = await PublicKey.findProgramAddress([config_seed], programId);
       keys[i] = {pubkey: pda, isSigner: false, isWritable: true}
       bumps[i] = bump;
-      console.log(pda.toBase58(),bump)
+      console.log(pda.toBase58(),bump,"pda config")
     }
 
     //metadata account pda
@@ -86,18 +88,18 @@ async function main() {
       const [pda, bump] = await PublicKey.findProgramAddress([b1,MPL_TOKEN_METADATA_ID.toBytes(),b3], MPL_TOKEN_METADATA_ID);
       keys[i] = {pubkey: pda, isSigner: false, isWritable: true}
       bumps[i] = bump;
-      console.log(pda.toBase58(),bump)
+      console.log(pda.toBase58(),bump,"pda metadata")
     }
 
     //mint account pda
     if (i==11){
-      // let mint_seed = Buffer.from("mint")
-      const mint_seed = Buffer.from([109, 105, 110, 116]);
+      let mint_seed = Buffer.from("mint")
+      // const mint_seed = Buffer.from([109, 105, 110, 116]);
       const noise = Buffer.from([89, 157, 88, 232, 243, 249, 197, 132, 199, 49, 19, 234, 91, 94, 150, 41].slice());
       const [pda, bump] = await PublicKey.findProgramAddress([mint_seed, noise], programId);
       keys[i] = {pubkey: pda, isSigner: false, isWritable: true}
       bumps[i] = bump;
-      console.log(pda.toBase58(),bump)
+      console.log(pda.toBase58(),bump,"pda mint")
     }
 
     //treasury account pda
@@ -106,31 +108,37 @@ async function main() {
       const [pda, bump] = await PublicKey.findProgramAddress([treasury_seed], programId);
       keys[i] = {pubkey: pda, isSigner: false, isWritable: true}
       bumps[i] = bump;
-      console.log(pda.toBase58(),bump)
+      console.log(pda.toBase58(),bump,"pda treasury")
     }
-
+    // treasury_tokens, 通过TREASURY_TOKENS_ADDRESS复杂的方式算出来的
     if (i==13){
       keys[i]={pubkey: new PublicKey("DnFwu1LKF7WqnKvrYAVKLoqQ8M1Asz2BN8CRqbYXKATF"), isSigner: false, isWritable: true}
+      console.log(keys[i].pubkey.toBase58(),"TREASURY_TOKENS_ADDRESS")
     }
     //system_program account 11111111111111111111111111111111
     if (i==14){
       keys[i]={pubkey: new PublicKey("11111111111111111111111111111111"), isSigner: false, isWritable: true}
+      // console.log(keys[i].pubkey.toBase58(),"system_program")
     }
     //token_program account TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA
     if (i==15){
       keys[i]={pubkey: new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"), isSigner: false, isWritable: true}
+      console.log(keys[i].pubkey.toBase58(),"token_program")
     }
     //associated_token account ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL
     if (i==16){
       keys[i]={pubkey: new PublicKey("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"), isSigner: false, isWritable: true}
+      console.log(keys[i].pubkey.toBase58(),"associated_token")
     }
     //metadata_program account
     if (i==17){
       keys[i]={pubkey: new PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"), isSigner: false, isWritable: true}
+      console.log(keys[i].pubkey.toBase58(),"metadata_program")
     }
     //rent_sysvar account
     if (i==18){
       keys[i]={pubkey: new PublicKey("SysvarRent111111111111111111111111111111111"), isSigner: false, isWritable: true}
+      console.log(keys[i].pubkey.toBase58(),"rent_sysvar")
     }
   }
 
@@ -142,23 +150,24 @@ async function main() {
   }
 
 
+  console.log(userKeypair.publicKey.toBase58(),"signer")
 
   const instruction = new TransactionInstruction({keys: keys, programId, data: data,});
 
-  console.log("=======","ready to init");
-  await sendAndConfirmTransaction(
-      connection,
-      new Transaction().add(instruction).add(ComputeBudgetProgram.setComputeUnitLimit({ units: 18_00_000 })),
-      [userKeypair],
-  ).then(r=>{
-    console.log("=======","50","program init success",r);
-  }).catch(e=>{
-    console.log("=======","50","program init err",e);
-  })
+  console.log("=======","ready to",func.name);
+  // await sendAndConfirmTransaction(
+  //     connection,
+  //     new Transaction().add(instruction).add(ComputeBudgetProgram.setComputeUnitLimit({ units: 18_00_000 })),
+  //     [userKeypair],
+  // ).then(r=>{
+  //   console.log("=======",func.name,"success",r);
+  // }).catch(e=>{
+  //   console.log("=======",func.name,"err",e);
+  // })
 }
 
 
-main().then(
+process_initialize().then(
   () => process.exit(),
   err => {
     console.error(err);
